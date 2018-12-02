b695e18836c49ba2a92f0cca1791d7087eafaeed
udcun <duphungcong@gmail.com>
2018-12-02 17:57:46 +0700

    be/create models for ams app

--- ams/models.py
+++ ams/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Aircraft(models.Model):
+    register = models.CharField(max_length=10)
+    AIRCRAFT_TYPE_CHOICES = (
+        (1, 'ATR72'),
+        (2, 'A320'),
+        (3, 'A321'),
+        (4, 'B787'),
+        (5, 'B777'),
+        (6, 'A330'),
+        (7, 'A350'),
+    )
+    type = models.IntegerField(
+        choices=AIRCRAFT_TYPE_CHOICES,
+        default=1,
+    )
+
+    def __str__(self):
+        return self.register
+
+
+class Task(models.Model):
+    number = models.CharField(max_length=20)
+    zone = models.CharField(max_length=10)
+    code = models.CharField(max_length=3)
+    title = models.CharField
+    ams_mh = models.FloatField
+    actual_mh = models.FloatField
+    men = models.IntegerField
+    hour = models.FloatField
+
+    def __str__(self):
+        return self.number
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=10)
+    main_area = models.ForeignKey('self', null=True, related_name='area', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Tool(models.Model):
+    name = models.CharField(max_length=20)
+    pn = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.pn
+
+
+class Spare(models.Model):
+    name = models.CharField(max_length=20)
+    pn = models.CharField(max_length=20)
+    SPARE_TYPE_CHOICES = (
+        (1, 'EP'),
+        (2, 'CO'),
+        (3, 'AS')
+    )
+    type = models.IntegerField(
+        choices=SPARE_TYPE_CHOICES,
+        default=1
+    )
+
+    def __str__(self):
+        return self.pn
+
+
+class Note(models.Model):
+    content = models.CharField
--- sgnbase/urls.py
+++ sgnbase/urls.py
@@ -15,13 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
-from  rest_framework_jwt.views import verify_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+
+from ams import views
+
+router = routers.DefaultRouter()
+router.register(r'aircrafts', views.AircraftViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
     url(r'^auth/obtain-token', obtain_jwt_token),
     url(r'^auth/refresh-token', refresh_jwt_token),
     url(r'^auth/verify-token', verify_jwt_token),
